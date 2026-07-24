8e4e62525e104d2c1a7a2e7a232656b25217095f
FishKepping <cphayer1@gmail.com>
2026-07-25 08:50:25 +1200

    Create LeafSense core library structure

---
+++ leafsense-core/include/leafsense/thermal_frame.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace leafsense {
+
+class ThermalFrame {
+public:
+    ThermalFrame() = default;
+};
+
+} // namespace leafsense
---
+++ leafsense-core/src/thermal_frame.cpp
@@ -0,0 +1,5 @@
+#include "leafsense/thermal_frame.h"
+
+namespace leafsense {
+
+} // namespace leafsense
